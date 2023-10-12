2e6a324514ae19cb8bd49e6f0ae669b658731570
SahilBShah <sahil2699@gmail.com>
2023-10-11 22:58:09 -0700

    Updated notebooks

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # b2_infection
+
+##Correlate (enrichment) B2 SINE position to mRNA isoform difference
+###If we can figure this out?
+###B2 position to expression - isoform and gene level
+##Comparison of how many genes are DE in Brf1 KD vs. Ctrl Inf. and how many have B2 in them?
+##How many genes have B2s in them in the genome?
+##How many genes are induced during infection contain a B2 SINE?
+###Highlight based on B2 SINE position in gene
+###Volcano plot
+##How many genes are induced during infection in a Pol3-dependent manner?
+###Ctrl inf vs Ctrl mock
+###Brf1 vs Ctrl inf.
+###Bar graph to display this data
+###At isoform level too - use all mRNA isoforms % as reference
+##Look at the background distribution of B2 SINEs and compare it to our empirical data
+##Is there a stop codon in the mRNA isoforms with retained introns?
+##Send Azra isoform usage data w/ B2 location and gene
+##GO term with DE isoforms with a B2
+##Average size of B2 SINE?
+###Intact B2 = ~180bp
+##Distance analyses?
+###Eg. distance of B2 SINE from nearest exon and its effect on mRNA isoform
--- r_scripts/make_volcano_plot.R
+++ r_scripts/make_volcano_plot.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(cowplot)
 library(dplyr)
+library(stringr)
 
 setwd("/Users/sahilshah/Documents/b2_infection/data/DE_analysis")
 
@@ -162,3 +163,125 @@ ggplot(ctrl_48_mock, aes(x=logFC, y=-log(adj.P.Val,10))) + # -log10 conversion
   theme_cowplot()
 
 ggsave("../../figures/ctrl_48_mock_DE_genes.png")
+
+
+#Volcano plot of Ctrl conditions colored based on B2 location
+isoform_b2_loc <- read.csv("intact_b2_mapped_to_isoforms.csv")
+colnames(isoform_b2_loc) <- c("Index", "Transcript", "B2_location")
+isoform_b2_loc <- subset(isoform_b2_loc, select = -c(Index))
+isoform_b2_loc <- isoform_b2_loc %>% distinct()
+
+##Brf1 48hpi vs. Ctrl 48hpi
+brf1_ctrl_inf_b2 <- left_join(brf1_ctrl_inf, isoform_b2_loc, by=join_by(Transcript == Transcript))
+brf1_ctrl_inf_b2[is.na(brf1_ctrl_inf_b2)] <- "None/Undetermined"
+ggplot(brf1_ctrl_inf_b2, aes(x=logFC, y=-log(adj.P.Val,10), label=B2_location)) + # -log10 conversion  
+  geom_point(size = 2/5, aes(color=B2_location), show.legend=TRUE) +
+  xlab(expression("log"[2]*"FC")) + 
+  ylab(expression("-log"[10]*"FDR")) +
+  ggtitle("Brf1 48hpi vs. Ctrl 48hpi") +
+  geom_hline(yintercept=1.3, linetype="dashed", color="red") +
+  geom_vline(xintercept=1.5, linetype="dashed", color="red") +
+  geom_vline(xintercept=-1.5, linetype="dashed", color="red") +
+  scale_color_manual(values=c("black", "blue", "green", "orange")) +
+  theme_cowplot()
+
+ggsave("../../figures/brf1_ctrl_inf_b2_mapped_to_DE_isoforms.png")
+
+#Intact B2s mapped to all isoforms
+all_iso_introns <- read.delim("../bed_files/All_isoforms_intactB2_mapped_to_introns.bed", sep = "\t", header=FALSE)
+all_iso_introns <- subset(all_iso_introns, select = c(V4))
+all_iso_introns <- as.data.frame(str_split_fixed(all_iso_introns$V4, '_', 2))
+all_iso_introns <- subset(all_iso_introns, select = c(V1))
+all_iso_introns <- all_iso_introns %>% distinct()
+colnames(all_iso_introns) <- c("Transcript")
+
+all_iso_exons <- read.delim("../bed_files/All_isoforms_intactB2_mapped_to_exons.bed", sep = "\t", header=FALSE)
+all_iso_exons <- as.data.frame(all_iso_exons$V4)
+colnames(all_iso_exons) <- c("Transcript")
+
+all_iso_5prime <- read.delim("../bed_files/All_isoforms_intactB2_mapped_to_5prime_UTR.bed", sep = "\t", header=FALSE)
+all_iso_5prime <- as.data.frame(all_iso_5prime$V4)
+colnames(all_iso_5prime) <- c("Transcript")
+
+all_iso_3prime <- read.delim("../bed_files/All_isoforms_intactB2_mapped_to_3prime_UTR.bed", sep = "\t", header=FALSE)
+all_iso_3prime <- as.data.frame(all_iso_3prime$V4)
+colnames(all_iso_3prime) <- c("Transcript")
+
+all_iso_prom <- read.delim("../bed_files/All_isoforms_intactB2_mapped_to_promoter_up4kb.bed", sep = "\t", header=FALSE)
+all_iso_prom <- as.data.frame(all_iso_prom$V4)
+colnames(all_iso_prom) <- c("Transcript")
+
+B2_location <- rep(c("Intron", "Exon", "5primeUTR", "3primeUTR", "Promoter"), 
+                   times=c(length(all_iso_introns$Transcript), length(all_iso_exons$Transcript),
+                           length(all_iso_5prime$Transcript), length(all_iso_3prime$Transcript),
+                           length(all_iso_prom$Transcript)))
+Transcript <- rbind(all_iso_introns, all_iso_exons, all_iso_5prime, all_iso_3prime, all_iso_prom)
+
+all_iso_b2_loc <- data.frame(Transcript, B2_location)
+all_iso_b2_loc <- all_iso_b2_loc %>%  group_by(Transcript) %>% mutate(B2_location = paste0(B2_location, collapse = "_"))
+all_iso_b2_loc$B2_location <- ifelse(grepl("_",all_iso_b2_loc$B2_location), "Multiple B2s", all_iso_b2_loc$B2_location)
+all_iso_b2_loc <- all_iso_b2_loc %>% distinct()
+
+##Brf1 48hpi vs. Ctrl 48hpi
+brf1_ctrl_inf_b2 <- left_join(brf1_ctrl_inf, all_iso_b2_loc, by=join_by(Transcript == Transcript))
+brf1_ctrl_inf_b2[is.na(brf1_ctrl_inf_b2)] <- "None"
+ggplot(brf1_ctrl_inf_b2, aes(x=logFC, y=-log(adj.P.Val,10), label=B2_location)) + # -log10 conversion  
+  geom_point(size = 2/5, aes(color=B2_location), show.legend=TRUE) +
+  xlab(expression("log"[2]*"FC")) + 
+  ylab(expression("-log"[10]*"FDR")) +
+  ggtitle("Brf1 48hpi vs. Ctrl 48hpi") +
+  geom_hline(yintercept=1.3, linetype="dashed", color="red") +
+  geom_vline(xintercept=1.5, linetype="dashed", color="red") +
+  geom_vline(xintercept=-1.5, linetype="dashed", color="red") +
+  scale_color_manual(values=c("red", "blue", "green", "orange", "purple", "black", "brown")) +
+  theme_cowplot()
+
+ggsave("../../figures/brf1_ctrl_inf_b2_mapped_to_ALL_isoforms.png")
+
+# ##Brfl mock vs. Ctrl mock
+# brf1_ctrl_mock_b2 <- left_join(brf1_ctrl_mock, all_iso_b2_loc, by=join_by(Transcript == Transcript))
+# brf1_ctrl_mock_b2[is.na(brf1_ctrl_mock_b2)] <- "None"
+# ggplot(brf1_ctrl_mock_b2, aes(x=logFC, y=-log(adj.P.Val,10), label=B2_location)) + # -log10 conversion  
+#   geom_point(size = 2/5, aes(color=B2_location), show.legend=TRUE) +
+#   xlab(expression("log"[2]*"FC")) + 
+#   ylab(expression("-log"[10]*"FDR")) +
+#   ggtitle("Brf1 mock vs. Ctrl mock") +
+#   geom_hline(yintercept=1.3, linetype="dashed", color="red") +
+#   geom_vline(xintercept=1.5, linetype="dashed", color="red") +
+#   geom_vline(xintercept=-1.5, linetype="dashed", color="red") +
+#   scale_color_manual(values=c("black", "blue", "green", "orange")) +
+#   theme_cowplot()
+# 
+# ggsave("../../figures/brf1_ctrl_mock_b2_mapped_to_ALL_isoforms.png")
+
+# ##Brf1 48hpi vs. Brf1 mock
+# brf1_48_mock_b2 <- left_join(brf1_48_mock, all_iso_b2_loc, by=join_by(Transcript == Transcript))
+# brf1_48_mock_b2[is.na(brf1_48_mock_b2)] <- "None"
+# ggplot(brf1_48_mock_b2, aes(x=logFC, y=-log(adj.P.Val,10), label=B2_location)) + # -log10 conversion  
+#   geom_point(size = 2/5, aes(color=B2_location), show.legend=TRUE) +
+#   xlab(expression("log"[2]*"FC")) + 
+#   ylab(expression("-log"[10]*"FDR")) +
+#   ggtitle("Brf1 48hpi vs. Brf1 mock") +
+#   geom_hline(yintercept=1.3, linetype="dashed", color="red") +
+#   geom_vline(xintercept=1.5, linetype="dashed", color="red") +
+#   geom_vline(xintercept=-1.5, linetype="dashed", color="red") +
+#   scale_color_manual(values=c("black", "blue", "green", "orange")) +
+#   theme_cowplot()
+# 
+# ggsave("../../figures/brf1_48_mock_b2_mapped_to_ALL_isoforms.png")
+# 
+# ##Ctrl 48hpi vs. Ctrl mock
+# ctrl_48_mock_b2 <- left_join(ctrl_48_mock, all_iso_b2_loc, by=join_by(Transcript == Transcript))
+# ctrl_48_mock_b2[is.na(ctrl_48_mock_b2)] <- "None"
+# ggplot(ctrl_48_mock_b2, aes(x=logFC, y=-log(adj.P.Val,10), label=B2_location)) + # -log10 conversion  
+#   geom_point(size = 2/5, aes(color=B2_location), show.legend=TRUE) +
+#   xlab(expression("log"[2]*"FC")) + 
+#   ylab(expression("-log"[10]*"FDR")) +
+#   ggtitle("Ctrl 48hpi vs. Ctrl mock") +
+#   geom_hline(yintercept=1.3, linetype="dashed", color="red") +
+#   geom_vline(xintercept=1.5, linetype="dashed", color="red") +
+#   geom_vline(xintercept=-1.5, linetype="dashed", color="red") +
+#   scale_color_manual(values=c("black", "blue", "green", "orange")) +
+#   theme_cowplot()
+# 
+# ggsave("../../figures/ctrl_48_mock_b2_mapped_to_ALL_isoforms.png")
